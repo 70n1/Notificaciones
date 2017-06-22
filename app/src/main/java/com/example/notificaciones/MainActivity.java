96b1895954178d9f7cf65901039ca0502401731d
70n1 <el.70n1@gmail.com>
2017-06-22 19:46:00 +0200

    Añadir una tercera página en la notificación

--- app/src/main/java/com/example/notificaciones/MainActivity.java
+++ app/src/main/java/com/example/notificaciones/MainActivity.java
@@ -38,14 +38,30 @@ public class MainActivity extends AppCompatActivity {
                 int notificacionId = 001;
 
                 NotificationCompat.Action accion = new NotificationCompat.Action.Builder(R.mipmap.ic_action_call, "llamar Wear", intencionPendienteLlamar).build();
-                List<NotificationCompat.Action> acciones = new ArrayList<NotificationCompat.Action>(); acciones.add(accion);
+                List<NotificationCompat.Action> acciones = new ArrayList<NotificationCompat.Action>();
+                acciones.add(accion);
                 acciones.add(new NotificationCompat.Action(R.mipmap.ic_action_locate, "Ver mapa", intencionPendienteMapa));
 
 
-                String s="Texto largo con descripción detallada de la notificación. ";
+                String s = "Texto largo con descripción detallada de la notificación. ";
 
-                NotificationCompat.WearableExtender wearableExtender = new NotificationCompat.WearableExtender() .setHintHideIcon(true) .setBackground(BitmapFactory.decodeResource( getResources(), R.drawable.escudo_upv))
-                        .addActions(acciones);;
+
+                // Creamos un BigTextStyle para la segunda página
+                NotificationCompat.BigTextStyle segundaPg = new NotificationCompat.BigTextStyle();
+                segundaPg.setBigContentTitle("Página 2").bigText("Más texto.");
+
+                // Creamos una notification para la segunda página
+                Notification notificacionPg2 = new NotificationCompat.Builder(MainActivity.this).setStyle(segundaPg).build();
+
+                NotificationCompat.BigTextStyle terceraPg = new NotificationCompat.BigTextStyle();
+                terceraPg.setBigContentTitle("Página 3").bigText("Texto 3era página.");
+
+                // Creamos una notification para la segunda página
+                Notification notificacionPg3 = new NotificationCompat.Builder(MainActivity.this).setStyle(terceraPg).build();
+
+
+                NotificationCompat.WearableExtender wearableExtender = new NotificationCompat.WearableExtender().setHintHideIcon(true).setBackground(BitmapFactory.decodeResource(getResources(), R.drawable.escudo_upv))
+                        .addActions(acciones).addPage(notificacionPg2).addPage(notificacionPg3);
 
                 Notification notificacion = new NotificationCompat.Builder(MainActivity.this)
                         .setSmallIcon(R.mipmap.ic_launcher).setContentTitle("Título").setContentText(Html.fromHtml("<b>Notificación</b> <u>Android <i>Wear</i></u>")).setContentIntent(intencionPendienteMapa)
@@ -53,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
                         //.extend(new NotificationCompat.WearableExtender().addActions( acciones))
                         //.setLargeIcon(BitmapFactory.decodeResource( getResources(), R.drawable.escudo_upv))
                         .extend(wearableExtender)
-                        .setStyle(new NotificationCompat.BigTextStyle().bigText(s+s+s+s))
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText(s + s + s + s))
                         .build();
                 NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
                 notificationManager.notify(notificacionId, notificacion);
