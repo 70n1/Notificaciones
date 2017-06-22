fb52b4a560e760430019b213aee7cb9967579823
70n1 <el.70n1@gmail.com>
2017-06-22 19:06:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/notificaciones/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.notificaciones;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button wearButton = (Button) findViewById(R.id.boton1);
+        wearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intencionLlamar = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:555123456"));
+                PendingIntent intencionPendienteLlamar = PendingIntent.getActivity(MainActivity.this, 0, intencionLlamar, 0);
+
+                // Creamos intención pendiente
+                //Intent intencionMapa = new Intent(MainActivity.this, MainActivity.class);
+                Intent intencionMapa = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=universidad+politecnica+valencia"));
+                PendingIntent intencionPendienteMapa = PendingIntent.getActivity(MainActivity.this, 0, intencionMapa, 0);
+                int notificacionId = 001;
+                Notification notificacion = new NotificationCompat.Builder(MainActivity.this)
+                        .setSmallIcon(R.mipmap.ic_launcher).setContentTitle("Título").setContentText(Html.fromHtml("<b>Notificación</b> <u>Android <i>Wear</i></u>")).setContentIntent(intencionPendienteMapa)
+                        .addAction(R.mipmap.ic_action_call, "llamar", intencionPendienteLlamar)
+                        .build();
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
+                notificationManager.notify(notificacionId, notificacion);
+            }
+        });
+    }
+}
