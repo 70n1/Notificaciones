5c39845b834e7eaab66a9d83b03655b49e6a9d1f
70n1 <el.70n1@gmail.com>
2017-06-22 19:28:33 +0200

    Añadir un texto largo a una notificación

--- app/src/main/java/com/example/notificaciones/MainActivity.java
+++ app/src/main/java/com/example/notificaciones/MainActivity.java
@@ -41,10 +41,15 @@ public class MainActivity extends AppCompatActivity {
                 List<NotificationCompat.Action> acciones = new ArrayList<NotificationCompat.Action>(); acciones.add(accion);
                 acciones.add(new NotificationCompat.Action(R.mipmap.ic_action_locate, "Ver mapa", intencionPendienteMapa));
 
+
+                String s="Texto largo con descripción detallada de la notificación. ";
+
+
                 Notification notificacion = new NotificationCompat.Builder(MainActivity.this)
                         .setSmallIcon(R.mipmap.ic_launcher).setContentTitle("Título").setContentText(Html.fromHtml("<b>Notificación</b> <u>Android <i>Wear</i></u>")).setContentIntent(intencionPendienteMapa)
                         .addAction(R.mipmap.ic_action_call, "llamar", intencionPendienteLlamar).extend(new NotificationCompat.WearableExtender().addActions( acciones))
                         .setLargeIcon(BitmapFactory.decodeResource( getResources(), R.drawable.escudo_upv))
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText(s+s+s+s))
                         .build();
                 NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
                 notificationManager.notify(notificacionId, notificacion);
